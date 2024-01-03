31dab01d4a838a0103df4613f0f5c37c99c52c0a
Epoza <86025026+Epoza@users.noreply.github.com>
2024-01-02 22:24:21 -0500

    create prompt and hover

--- public/script.js
+++ public/script.js
@@ -1,10 +1,21 @@
 "use strict";
 let containerEl = document.querySelector(".div_container");
-let divRowsCols = 4; // make this a prompt later
-let divDimensions = divRowsCols * 25;
+let userInput = parseInt(prompt("Enter amount rows x columns", "4"));
+let divRowsCols;
+// checks that userInput is a whole number from 1-99
+if (userInput >= 1 || userInput <= 99) {
+    divRowsCols = userInput;
+    console.log(divRowsCols);
+}
+else {
+    alert("Please enter a number from 1 to 99");
+    divRowsCols = 4;
+}
+let testing123 = 100 / divRowsCols;
+let divDimensions = divRowsCols * testing123;
 // function that calculates grid col and rows
 function createRowsCols() {
-    containerEl.style.gridTemplateColumns = `repeat(${divRowsCols}, 1fr)`; // change 4 to whatever divrowscols
+    containerEl.style.gridTemplateColumns = `repeat(${divRowsCols}, auto)`; // change 4 to whatever divrowscols
     containerEl.style.gridTemplateRows = `repeat(${divRowsCols}, ${divDimensions}px)`; // change px to match height and width using divrowscols * 6
     containerEl.style.gap = "2px"; // delete later
 }
@@ -13,9 +24,16 @@ function createDivs(divRowsCols) {
     let totalDivs = divRowsCols * divRowsCols; // gets total divs ex: 16 = 4 * 4
     for (let i = 0; i < totalDivs; i++) {
         let newDiv = document.createElement("div");
-        newDiv.style.background = "black";
+        newDiv.style.background = "white";
         newDiv.style.height = `${divDimensions}px`; // multiply divrowscols * 6
         newDiv.style.width = `${divDimensions}px`; // multiply divrowscols * 6
+        // changes background color when hovered
+        newDiv.addEventListener("mouseover", (e) => {
+            if (e.target) {
+                const targetElement = e.target;
+                targetElement.style.backgroundColor = "black";
+            }
+        });
         containerEl.appendChild(newDiv);
     }
 }
--- src/script.ts
+++ src/script.ts
@@ -1,10 +1,23 @@
 let containerEl = document.querySelector(".div_container") as HTMLElement;
-let divRowsCols: number = 4; // make this a prompt later
-let divDimensions: number = divRowsCols * 25;
+let userInput = parseInt(prompt("Enter amount rows x columns", "4")!);
+
+let divRowsCols: number;
+// checks that userInput is a whole number from 1-99
+if(userInput >= 1 || userInput <= 99){
+    divRowsCols = userInput;
+    console.log(divRowsCols);
+} else {
+    alert("Please enter a number from 1 to 99")
+    divRowsCols = 4;
+}
+
+let testing123 = 100 / divRowsCols;
+
+let divDimensions: number = divRowsCols * testing123;
 
 // function that calculates grid col and rows
 function createRowsCols() {
-    containerEl.style.gridTemplateColumns = `repeat(${divRowsCols}, 1fr)`; // change 4 to whatever divrowscols
+    containerEl.style.gridTemplateColumns = `repeat(${divRowsCols}, auto)`; // change 4 to whatever divrowscols
     containerEl.style.gridTemplateRows = `repeat(${divRowsCols}, ${divDimensions}px)`; // change px to match height and width using divrowscols * 6
     containerEl.style.gap = "2px"; // delete later
 }
@@ -14,9 +27,16 @@ function createDivs(divRowsCols: number) {
     let totalDivs = divRowsCols * divRowsCols; // gets total divs ex: 16 = 4 * 4
     for(let i = 0; i < totalDivs; i++){
         let newDiv = document.createElement("div")
-        newDiv.style.background = "black";
+        newDiv.style.background = "white";
         newDiv.style.height = `${divDimensions}px`; // multiply divrowscols * 6
         newDiv.style.width = `${divDimensions}px`;    // multiply divrowscols * 6
+        // changes background color when hovered
+        newDiv.addEventListener("mouseover", (e: MouseEvent) => {
+            if (e.target) {
+                const targetElement = e.target as HTMLElement;
+                targetElement.style.backgroundColor = "black";
+            }
+        });
         containerEl.appendChild(newDiv);
     }
 }
@@ -24,4 +44,5 @@ createRowsCols();
 createDivs(divRowsCols)
 
 // event listener hover function
+
 // change background color to black
